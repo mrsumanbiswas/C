58fcc209d194c19c5a123f12d499414abd394815
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-10-25 03:51:56 +0530

    update a.out and main.c

--- assignments/01/main.c
+++ assignments/01/main.c
@@ -3,71 +3,77 @@
 
 #define MODE "debug"
 
-void analyse(int , char *[]);
-void gen_file(int , char *[]);
+void analyse(int, char *[]);
+void gen_file(int, char *[]);
 
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
-    (argc > 1) ? analyse(argc,argv):printf("Please give the items as argument.\n");
+    (argc > 1) ? analyse(argc, argv) : printf("Please give the items as argument.\n");
 
     // for debug purpuse only
     if (MODE != "debug")
-    if (argc > 1)
-    {
-        for (int i = 1; i < argc; i++)
+        if (argc > 1)
         {
-            printf("%s\n", argv[i]);
-        }        
-    } else{
-        printf("%d\n", argc);
-    }
+            for (int i = 1; i < argc; i++)
+            {
+                printf("%s\n", argv[i]);
+            }
+        }
+        else
+        {
+            printf("%d\n", argc);
+        }
 
     return 0;
 }
 
-
-
-// analyse the words || this is the main logic 
-void analyse(int len, char *words[]){
+// analyse the words || this is the main logic
+void analyse(int len, char *words[])
+{
     for (int i = 1; i < len; i++)
     {
-        if (words[i][0]=='A')
+        if (words[i][0] == 'A')
         {
-            words[i][0]=' ';
-        printf("Add -> %s\n",words[i]);
-
-        }else if (words[i][0]=='D')
-        {
-            words[i][0]=' ';
-            printf("Delete -> %s\n",words[i]);
+            words[i][0] = ' ';
+            printf("Add -> %s\n", words[i]);
         }
-        
-        
+        else if (words[i][0] == 'D')
+        {
+            words[i][0] = ' ';
+            printf("Delete -> %s\n", words[i]);
+        }
     }
-    gen_file(len,words);
+    gen_file(len, words);
 }
 
-// generates a `files.txt` 
-void gen_file(int len, char *words[]){
+// generates a `files.txt`
+void gen_file(int len, char *words[])
+{
     FILE *fp;
-    int col;
-    char** lines;
+    int col = 0;
+    char lines[1000][1000];
 
     // reading the file
-    fp = fopen("list.txt","r");
+    fp = fopen("list.txt", "r");
     char con[1000];
-    while (fgets(con,1000, fp)!=NULL)// reading file content
-        printf("%s",con);
-        // lines[col] = con;
+    while (fgets(con, 1000, fp) != NULL) // reading file content
+    {
+
+        // printf("%d::%s", col, con);
+        strcpy(lines[col],con);
+        printf("%s",lines[col]);
         col++;
+    }
 
     fclose(fp); // closing of file
 
     // updating the file
-    fp = fopen("list.txt","w");
-    printf("%d",col);
-    for (int x=0; x <col; x++){
-        fprintf(fp,"%s\n", lines[x]); // giving conten
+    fp = fopen("list.txt", "w");
+    printf("%d", col);
+    for (int x = 0; x < col; x++)
+    {
+        // printf("hi -> %s\n",line);
+        fprintf(fp, "%s\n",lines[x] ); // giving conten
     }
     fclose(fp); // closing of file
 }
