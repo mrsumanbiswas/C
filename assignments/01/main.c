8a37a44175978888b571038ee854f002f1bc35c0
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-10-25 00:27:17 +0530

    update a.out and main.c

--- assignments/01/main.c
+++ assignments/01/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-char *get_words(){}
-void analyse(char const words[]){}
-void gen_file(char const words[]){}
+#define MODE "debugd"
+
+
+char *get_words();
+void analyse(const char *);
+void gen_file(char *);
 
 int main(int argc, char const *argv[])
 {
     (argc > 1) ? analyse(*argv):analyse(get_words(*argv));
 
     // for debug purpuse only
+    if (MODE == "debug")
     if (argc > 1)
     {
         for (size_t i = 1; i < argc; i++)
@@ -18,21 +22,23 @@ int main(int argc, char const *argv[])
     } else{
         printf("%d\n", argc);
     }
-    
+
     return 0;
 }
 
 // gets words from console
 char *get_words(){
-
+    char text[5];
+    // getline();
+    return &text;
 }
 
 // analyse the words || this is the main logic 
-void analyse(char const words[]){
+void analyse(const char words[]){
 
 }
 
 // generates a `files.txt` 
-void gen_file(char const words[]){
+void gen_file(char words[]){
 
 }
