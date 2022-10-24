2f07fe39c7350cc40d4ab3ebaa09a70d0dba6926
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-10-25 02:00:07 +0530

    update a.out and main.c

--- assignments/01/main.c
+++ assignments/01/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MODE "debug"
 
-char* get_words(int);
-void analyse(int ,const char[]);
-void gen_file(int ,const char[]);
+ char** get_words(int);
+void analyse(int , char *[]);
+void gen_file(int , char[]);
 
-int main(int argc, char const *argv[])
+int main(int argc, char  *argv[])
 {
-    (argc > 1) ? analyse(argc,*argv):analyse(argc,get_words(argc));
+    (argc > 1) ? analyse(argc,argv):analyse(argc,get_words(argc));
 
     // for debug purpuse only
-    if (MODE == "debug")
+    if (MODE != "debug")
     if (argc > 1)
     {
         for (int i = 1; i < argc; i++)
@@ -26,26 +27,28 @@ int main(int argc, char const *argv[])
 }
 
 // gets words from console
-char* get_words(int len){
+ char** get_words(int len){
     char text[]="asdf sdf s";
-    for (int i = 0; i < 0; i++)
-    {
-
-        text[i] = 's';
-        printf("%c\n",text[i]);
+    char* words = strtok(text," ");
+    while( strtok != NULL){
+        words = strtok(NULL," ");
+        printf("%s\n",words);
+        len++;
+        printf("%d\n",len);
     }
-    len = 5;
-    // getline();
-    // printf("%s",text);
-    return text;
+    return &words;
 }
 
 // analyse the words || this is the main logic 
-void analyse(int len,const char words[]){
-    printf("%d",len);
+void analyse(int len, char *words[]){
+    for (int i = 1; i < len; i++)
+    {
+        printf("at %d -> %s\n",i,words[i]);
+    }
+    
 }
 
 // generates a `files.txt` 
-void gen_file(int len,const char words[]){
+void gen_file(int len, char words[]){
 
 }
