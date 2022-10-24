e3d8b08bebfaf255db19ba30829e130094ee482e
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-10-25 00:12:05 +0530

    update main.c

--- assignments/01/main.c
+++ assignments/01/main.c
@@ -6,18 +6,19 @@ void gen_file(char const words[]){}
 
 int main(int argc, char const *argv[])
 {
+    (argc > 1) ? analyse(*argv):analyse(get_words(*argv));
+
+    // for debug purpuse only
     if (argc > 1)
     {
         for (size_t i = 1; i < argc; i++)
         {
             printf("%s\n", argv[i]);
-        }
-        
+        }        
     } else{
         printf("%d\n", argc);
-        analyse(get_words());
     }
-    (argc > 1) ? analyse(*argv):analyse(get_words(*argv));
+    
     return 0;
 }
 
