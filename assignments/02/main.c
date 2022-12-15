370025b522fe994d56119686137ed9422d464a8c
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-12-15 23:18:40 +0530

    update main.c

--- assignments/02/main.c
+++ assignments/02/main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+int main()
+{
+    int lines = 0;
+    char text[lines][1024], fileName[25];
+    char YN;
+
+    printf("How many lines do you want to write ?\n> ");
+    scanf("%d", &lines);
+
+    if (lines)
+    {
+        char temp[256];
+        for (int i = 0; i < lines; i++)
+        {
+            printf("%d | ", i + 1);
+            scanf("%s", temp);
+            strcpy(text[i], temp);
+        }
+    }
+    else
+    {
+        printf("No lines to write\n");
+    }
+
+    printf("Do you want to save it ?(Y/N)\n> ");
+    scanf("%s", &YN);
+
+    if (YN == 'N' || YN == 'n')
+    {
+        printf("s");
+    }
 
     return 0;
 }
